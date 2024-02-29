8e1626fc44f23aa30dc2491e523034bd6198f85b
jimiller20 <jmiller20twenty@gmail.com>
2024-02-28 20:42:05 -0600

    added back-end spring

---
+++ back-end/src/main/java/com/nomadnirvana/sloths/SlothsApplication.java
@@ -0,0 +1,13 @@
+package com.nomadnirvana.sloths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlothsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlothsApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/nomadnirvana/sloths/SlothsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomadnirvana.sloths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlothsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
